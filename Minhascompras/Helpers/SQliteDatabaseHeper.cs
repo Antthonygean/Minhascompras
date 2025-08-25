2af1332f1ec3c9d9f3999fb8350476cc5816b5a6
antthony <antthonygeandasilvaalves@gmail.com>
2025-08-25 07:07:46 -0300

    construcao da classe para fazer CRUID no SQlite

--- Minhascompras/Helpers/SQliteDatabaseHeper.cs
+++ Minhascompras/Helpers/SQliteDatabaseHeper.cs
@@ -3,10 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using Minhascompras.Models;
 
 namespace Minhascompras.Helpers
 {
-    internal class SQliteDatabaseHeper
+    public class SQliteDatabaseHeper
     {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQliteDatabaseHeper(string caminho)
+        {
+            _conn = new SQLiteAsyncConnection(caminho);
+            _conn.CreateTableAsync<Produto>().Wait();
+
+        }
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
+
+            return _conn.QueryAsync<Produto>(
+                sql, p.Descricao, p.Quantidade, p.Preco, p.Id
+            );
+        }
+
+        public Task<int> Delete(int id)
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string q)
+        {
+            string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%'";
+
+            return _conn.QueryAsync<Produto>(sql);
+        }
+
     }
 }
